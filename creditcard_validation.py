b1c991097be4b40fd3234ad3158e4b14348fb2c2
vk927 <vk927@nyu.edu>
2016-08-23 09:49:44 -0400

    test with pycharm

--- creditcard_validation.py
+++ creditcard_validation.py
@@ -1,2 +1,31 @@
+from functools import reduce
 import re
 import sys
+
+
+def cc_format(card_no):
+    card_no=card_no.replace(" ","")
+    card_no = card_no.replace("-","")
+    return card_no
+def xyz(l):
+    l=list(l)
+    l=map(int,l)
+    return reduce(lambda x,y:x+y,l)
+
+
+def cc_luhn(card_no):
+    odd=card_no[0::2]
+    even=card_no[1::2]
+
+    odd_sum=
+
+    even_sum=map(x,())
+
+
+
+
+if __name__=='__main__':
+    card_no=input("Enter the credit card number \n")
+    print("your credit card number is",card_no)
+    card_no=cc_format(card_no)
+    cc_luhn(card_no)
---
+++ ttt.py
@@ -0,0 +1,7 @@
+l=[1,2,3,4]
+def xy(i):
+    i=i*2
+    if i>9:
+
+r=map(lambda x:x*2,l,l)
+print(r[0])
