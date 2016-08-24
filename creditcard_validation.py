49526d4ca6d3fe511bc1ff86902b6203655f82f6
vk927 <vk927@nyu.edu>
2016-08-24 10:56:24 -0400

    test with pycharm

--- creditcard_validation.py
+++ creditcard_validation.py
@@ -1,3 +1,5 @@
+#this is damn complex way of writing a program
+#
 from functools import reduce
 import re
 import sys
@@ -7,25 +9,40 @@ def cc_format(card_no):
     card_no=card_no.replace(" ","")
     card_no = card_no.replace("-","")
     return card_no
-def xyz(l):
-    l=list(l)
+
+def reduce_cc(st):
+    l=list(str(st))
     l=map(int,l)
     return reduce(lambda x,y:x+y,l)
 
+def odd_double(i):
+    i=int(i)*2
+    if i>9:
+        i=reduce_cc(i)
+    return i
 
-def cc_luhn(card_no):
+def odd_map(st):
+    l=list(st)
+    l=map(odd_double,l)
+    return reduce(lambda x, y: x + y, l)
+
+def cc_luhn_check(card_no):
     odd=card_no[0::2]
     even=card_no[1::2]
 
-    odd_sum=
-
-    even_sum=map(x,())
-
-
-k
+    odd_sum=odd_map(odd)
+    even_sum=reduce_cc(even)
+    total_sum=odd_sum+even_sum
+    if total_sum%10==0:
+        return True
+    else:
+        return False
 
 if __name__=='__main__':
     card_no=input("Enter the credit card number \n")
     print("your credit card number is",card_no)
     card_no=cc_format(card_no)
-    cc_luhn(card_no)
+    if cc_luhn_check(card_no):
+        print("Credit Card Number is Valid")
+    else:
+        print("Credit Card Number is Invalid")
--- ttt.py
+++ ttt.py
@@ -1,7 +1,10 @@
-l=[1,2,3,4]
-def xy(i):
-    i=i*2
-    if i>9:
+visa = r'^4[0-9]{12}(?:[0-9]{3})?$'
+mastercard = r'^5[1-5][0-9]{14}$'
+am_express = r'^3[47][0-9]{13}$'
+diners_club = '^3(?:0[0-5]|[68][0-9])[0-9]{11}$'
+discover = r'^6(?:011|5[0-9]{2})[0-9]{12}$'
+jcb = r'^(?:2131|1800|35\d{3})\d{11}$'
 
-r=map(lambda x:x*2,l,l)
-print(r[0])
+cards = [visa, mastercard, am_express, diners_club, discover, jcb]
+
+print(cards[0])
