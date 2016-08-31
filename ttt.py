18d9705099edb84938f3271d13a83c8e7aa1e92f
vk927 <vk927@nyu.edu>
2016-08-31 09:50:40 -0400

    commit

--- ttt.py
+++ ttt.py
@@ -1,11 +1,2 @@
-import re
-pattern = r"(123)"
-string = "123zzb"
-
-match = re.match(pattern, string)
-print(match.groups(0))
-# Out: '123'
-
-sentence = "This is a phone number 672-123-456-9910"
-print(re.match(r'.*(phone).*?([\d-]+)', sentence).group(0))
-# Out: ('phone', '672-123-456-9910')
+for i in range(10):
+    print(i)
