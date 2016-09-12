0d430d7f4cf33ffe87ed9bb437235f3da390bdd6
vk927 <vk927@nyu.edu>
2016-09-12 15:33:31 -0400

    commit from pycharm

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+#this is bubble sort program
+#this program has complexity of O(N^2) complexity
+
+
+def bubble_sort(arr):
+    for i in range(len(arr)-1,0,-1):
+        for j in range(i):
+            if arr[j]>arr[j+1]:
+                tmp=arr[j+1]
+                arr[j+1]=arr[j]
+                arr[j]=tmp
+    return arr
+
+arr=[4,6,1,8,3]
+print(bubble_sort(arr))
